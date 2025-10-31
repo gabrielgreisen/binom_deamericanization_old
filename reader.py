64735c6374e4e2205963ab73f25583b636a85fcb
Advayth Pashupati <113481915+AamindMandragora@users.noreply.github.com>
2025-10-30 19:26:14 -0500

    added folders and cleaned up main.py

--- reader.py
+++ reader.py
@@ -5,11 +5,5 @@ def read_chain():
     df = pd.read_csv("spy_eod_202312.txt")
     return df
 
-def read_yfin():
-    df = yf.get_option_chains_all("AAPL")
-    return df
-
-calls, puts = read_yfin()
-df = calls
-
-print(df.columns)
+df = read_chain()
+print(df)
